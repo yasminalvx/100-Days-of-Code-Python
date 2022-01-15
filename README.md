99fd501052a80494ce1318fd061a9740d57955f8
Yasmin Alves <yasilvalves@gmail.com>
2022-01-14 22:44:13 -0300

    Day 1

---
+++ README.md
@@ -0,0 +1,11 @@
+## Curso 100 Days of Code: The Complete Python Pro Bootcampo for 2022
+
+<br>
+
+#### Domine Python construindo 100 projetos em 100 dias. Aprenda ciência de dados, automação, crie sites, jogos e aplicativos!
+
+<br>
+
+#### Por: <strong> Angela Yu </strong>
+
+Link: <a href="https://www.udemy.com/share/103IHM3@9bpD95_85n3q30V5SubtVAiz7dfxDbGYlvWS8VeknnJBy9x8hYgakvGGgvA-3kK_/"> 100 Days of Code: The Complete Python Pro Bootcamp for 2022 </a>
---
+++ day-001/band-name-genarator.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("Bem-vindo ao Gerador de nome de banda!")
+#2. Ask the user for the city that they grew up in.
+city = input("Qual é o nome da cidade em que você cresceu?\n")
+#3. Ask the user for the name of a pet.
+pet = input("Qual é o nome do seu pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("O nome da sua banda poderia ser: " + city + " " + pet)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
